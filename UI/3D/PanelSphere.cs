3d65e20787056cbd36f1ca216113188110a0f55c
ngdanghau <ngdghau201@gmail.com>
2021-05-03 21:06:57 +0700

    thêm panel thông số cho các hình cầu, hình hộp.

---
+++ UI/3D/PanelSphere.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace KTDH_Nhom23_DoAnCuoiKy.UI
+{
+    public partial class PanelSphere : UserControl
+    {
+        private static PanelSphere _instance;
+        public static PanelSphere Instance
+        {
+            get
+            {
+                if (_instance == null)
+                    _instance = new PanelSphere();
+                return _instance;
+            }
+        }
+
+        public decimal X
+        {
+            get { return x1.Value; }
+            set { x1.Value = value; }
+        }
+
+        public decimal Y
+        {
+            get { return y1.Value; }
+            set { y1.Value = value; }
+        }
+
+        public decimal Z
+        {
+            get { return z1.Value; }
+            set { z1.Value = value; }
+        }
+
+
+        public decimal Radius
+        {
+            get { return canh.Value; }
+            set { canh.Value = value; }
+        }
+
+        public PanelSphere()
+        {
+            InitializeComponent();
+        }
+    }
+}
