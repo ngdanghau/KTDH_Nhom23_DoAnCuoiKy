4d6abe88a6a1aadab09a4109c06c0856b9354b01
ngdanghau <ngdghau201@gmail.com>
2021-05-03 20:04:00 +0700

    Thêm thông số cho vẽ hình tam giác

--- UI/PanelTriangle.cs
+++ UI/PanelTriangle.cs
@@ -1,10 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
 using System.Windows.Forms;
 
 namespace KTDH_Nhom23_DoAnCuoiKy.UI
@@ -21,6 +15,43 @@ namespace KTDH_Nhom23_DoAnCuoiKy.UI
                 return _instance;
             }
         }
+
+        public decimal X1
+        {
+            get { return x1.Value; }
+            set { x1.Value = value; }
+        }
+
+        public decimal Y1
+        {
+            get { return y1.Value; }
+            set { y1.Value = value; }
+        }
+
+        public decimal X2
+        {
+            get { return x2.Value; }
+            set { x2.Value = value; }
+        }
+
+        public decimal Y2
+        {
+            get { return y2.Value; }
+            set { y2.Value = value; }
+        }
+
+        public decimal X3
+        {
+            get { return x3.Value; }
+            set { x3.Value = value; }
+        }
+
+        public decimal Y3
+        {
+            get { return y3.Value; }
+            set { y3.Value = value; }
+        }
+
         public PanelTriangle()
         {
             InitializeComponent();
