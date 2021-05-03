95f71283754466cf3a51faa290cfaee6d31e2dca
ngdanghau <ngdghau201@gmail.com>
2021-05-03 19:41:35 +0700

    thêm icon 3D và sửa click chọn

--- UI/Panel3DModel.cs
+++ UI/Panel3DModel.cs
@@ -1,16 +1,18 @@
-﻿using System;
+﻿using KTDH_Nhom23_DoAnCuoiKy.Properties;
+using KTDH_Nhom23_DoAnCuoiKy.Variables;
+using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
 using System.Windows.Forms;
+using static KTDH_Nhom23_DoAnCuoiKy.Variables.Constants;
 
 namespace KTDH_Nhom23_DoAnCuoiKy.UI
 {
     public partial class Panel3DModel : UserControl
     {
+        public static List<Button> lstBtnCalc = new List<Button>();
+        public static List<Image> lstBtn2 = new List<Image>();
+
         private static Panel3DModel _instance;
         public static Panel3DModel Instance
         {
@@ -21,9 +23,90 @@ namespace KTDH_Nhom23_DoAnCuoiKy.UI
                 return _instance;
             }
         }
+
+        public static void reset()
+        {
+            Init.ShapeCurrent = Shape.Sphere;
+            HandleInputPanel(PanelTriangle.Instance);
+            HandleButton(0, Resources._13_selected);
+        }
+
         public Panel3DModel()
         {
             InitializeComponent();
+        }
+
+        static void HandleButton(int index, Bitmap image)
+        {
+            for (int i = 0; i < lstBtnCalc.Count; i++)
+            {
+                if (i == index)
+                {
+                    lstBtnCalc[i].Image = image;
+                }
+                else
+                {
+                    lstBtnCalc[i].Image = lstBtn2[i];
+                }
+            }
+        }
+
+        static void HandleInputPanel(Control Panel)
+        {
+            if (!Form1.GroupBoxInput.Controls.Contains(Panel))
+            {
+                Form1.GroupBoxInput.Controls.Add(Panel);
+                Panel.Dock = DockStyle.Fill;
+                Panel.BringToFront();
+            }
+            else
+            {
+                Panel.BringToFront();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Init.ShapeCurrent = Shape.Sphere;
+            HandleInputPanel(PanelTriangle.Instance);
+            HandleButton(0, Resources._13_selected);
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            Init.ShapeCurrent = Shape.Cube;
+            HandleInputPanel(PanelTriangle.Instance);
+            HandleButton(2, Resources._14_selected);
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            Init.ShapeCurrent = Shape.Cone;
+            HandleInputPanel(PanelTriangle.Instance);
+            HandleButton(3, Resources._15_selected);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Init.ShapeCurrent = Shape.Cylinder;
+            HandleInputPanel(PanelTriangle.Instance);
+            HandleButton(1, Resources._16_selected);
+        }
+
+        private void Panel3DModel_Load(object sender, EventArgs e)
+        {
+            lstBtnCalc.Add(button1);
+            lstBtnCalc.Add(button5);
+            lstBtnCalc.Add(button9);
+            lstBtnCalc.Add(button10);
+
+            lstBtn2.Add(button1.Image);
+            lstBtn2.Add(button5.Image);
+            lstBtn2.Add(button9.Image);
+            lstBtn2.Add(button10.Image);
+
+            Init.ShapeCurrent = Shape.Line;
+            HandleButton(0, Resources._13_selected);
         }
     }
 }
